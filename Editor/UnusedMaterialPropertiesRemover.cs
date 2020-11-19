960ce66dfa3d5f363fde1ce94846990f7ef1ea57
momoma <momoma.creation@gmail.com>
2020-11-19 10:58:12 +0900

    Removed internal scripts

--- Editor/UnusedMaterialPropertiesRemover.cs
+++ Editor/UnusedMaterialPropertiesRemover.cs
@@ -4,35 +4,35 @@ using UnityEditor;
 namespace MomomaAssets
 {
 
-public class UnusedMaterialPropertiesRemover : Editor
-{
-    [MenuItem("MomomaTools/RemoveUnusedProperties")]
-    static void Remove()
+    public class UnusedMaterialPropertiesRemover : Editor
     {
-        var mats = (Material[])Resources.FindObjectsOfTypeAll(typeof(Material));
-        foreach (var mat in mats)
+        [MenuItem("MomomaTools/RemoveUnusedProperties")]
+        static void Remove()
         {
-            var so = new SerializedObject(mat);
-            so.Update();
+            var mats = Resources.FindObjectsOfTypeAll<Material>();
+            foreach (var mat in mats)
+            {
+                var so = new SerializedObject(mat);
+                so.Update();
 
-            var savedProp   = so.FindProperty("m_SavedProperties");
-            RemoveProperties(savedProp.FindPropertyRelative("m_TexEnvs"), mat);
-            RemoveProperties(savedProp.FindPropertyRelative("m_Floats"), mat);
-            RemoveProperties(savedProp.FindPropertyRelative("m_Colors"), mat);
-        
-            so.ApplyModifiedProperties();
+                var savedProp = so.FindProperty("m_SavedProperties");
+                RemoveProperties(savedProp.FindPropertyRelative("m_TexEnvs"), mat);
+                RemoveProperties(savedProp.FindPropertyRelative("m_Floats"), mat);
+                RemoveProperties(savedProp.FindPropertyRelative("m_Colors"), mat);
+
+                so.ApplyModifiedProperties();
+            }
+        }
+
+        static void RemoveProperties(SerializedProperty props, Material mat)
+        {
+            for (int i = props.arraySize - 1; i >= 0; i--)
+            {
+                var name = props.GetArrayElementAtIndex(i).FindPropertyRelative("first").stringValue;
+                if (!mat.HasProperty(name))
+                    props.DeleteArrayElementAtIndex(i);
+            }
         }
     }
-
-    static void RemoveProperties(SerializedProperty props, Material mat)
-    {
-        for (int i = props.arraySize - 1; i >= 0; i--)
-        {
-            var name = props.GetArrayElementAtIndex(i).FindPropertyRelative("first").stringValue;
-            if (!mat.HasProperty(name))
-                props.DeleteArrayElementAtIndex(i);
-        }
-    }
-}
 
 }// namespace MomomaAssets
