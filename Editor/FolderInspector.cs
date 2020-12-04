1547ebabb7fc27f25a5a296f90d743b8a0ae3b8b
momoma <momoma.creation@gmail.com>
2020-12-04 10:22:16 +0900

    Added folder inspector

---
+++ Editor/FolderInspector.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+using System.Text;
+using UnityEngine;
+using UnityEditor;
+
+namespace MomomaAssets
+{
+
+    [CustomEditor(typeof(DefaultAsset))]
+    class FolderInspector : Editor
+    {
+        string m_Text = null;
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            var path = AssetDatabase.GetAssetPath(target);
+            if (string.IsNullOrEmpty(path))
+                return;
+            if (!AssetDatabase.IsValidFolder(path))
+                return;
+            path = Path.GetDirectoryName(Application.dataPath) + '/' + path + "/.memo";
+            if (m_Text == null)
+            {
+                if (File.Exists(path))
+                    m_Text = File.ReadAllText(path, Encoding.UTF8);
+                else
+                    m_Text = "";
+            }
+            var oldEnabled = GUI.enabled;
+            try
+            {
+                using (var check = new EditorGUI.ChangeCheckScope())
+                {
+                    GUI.enabled = true;
+                    m_Text = EditorGUILayout.TextArea(m_Text);
+                    if (check.changed)
+                    {
+                        if (string.IsNullOrEmpty(m_Text))
+                            File.Delete(path);
+                        else
+                            File.WriteAllText(path, m_Text, Encoding.UTF8);
+                    }
+                }
+            }
+            finally
+            {
+                GUI.enabled = oldEnabled;
+            }
+        }
+    }
+
+}// namespace MomomaAssets
