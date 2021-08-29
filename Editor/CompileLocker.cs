8187d7797dd304edfcbed9c1849ed58c5f2d3898
momoma <momoma.creation@gmail.com>
2021-08-29 22:23:26 +0900

    Added CompileLocker

---
+++ Editor/CompileLocker.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+
+namespace MomomaAssets
+{
+    static class CompileLocker
+    {
+        const string menuPath = "MomomaTools/CompileLocked";
+        static bool isLocked = false;
+
+        [MenuItem(menuPath)]
+        static void ToggleLock()
+        {
+            if (isLocked)
+                EditorApplication.UnlockReloadAssemblies();
+            else
+                EditorApplication.LockReloadAssemblies();
+            isLocked = !isLocked;
+            Menu.SetChecked(menuPath, isLocked);
+        }
+    }
+}
