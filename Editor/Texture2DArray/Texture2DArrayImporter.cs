8511480aae3b518707011e204b8f39ddfd11b123
momoma <momoma.creation@gmail.com>
2022-10-10 23:44:59 +0900

    Fix T2DA editors

--- Editor/Texture2DArray/Texture2DArrayImporter.cs
+++ Editor/Texture2DArray/Texture2DArrayImporter.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using System.Collections.Generic;
 using UnityEditor;
 using UnityEditor.Experimental.AssetImporters;
-using UnityEditorInternal;
+using UnityEngine;
 
 namespace MomomaAssets
 {
-
     [ScriptedImporter(1, "texture2darray")]
-    public class Texture2DArrayImporter : ScriptedImporter
+    sealed class Texture2DArrayImporter : ScriptedImporter
     {
         [SerializeField]
         List<Texture2D> m_Texture2Ds = new List<Texture2D>();
@@ -44,6 +41,9 @@ namespace MomomaAssets
                 for (var index = 0; index < srcTexture2Ds.Count; ++index)
                 {
                     Graphics.CopyTexture(srcTexture2Ds[index], 0, texture2DArray, index);
+                    var srcPath = AssetDatabase.GetAssetPath(srcTexture2Ds[index]);
+                    if (!string.IsNullOrEmpty(srcPath))
+                        ctx.DependsOnSourceAsset(srcPath);
                 }
                 texture2DArray.filterMode = m_FilterMode;
                 texture2DArray.wrapModeU = m_WrapModeU;
@@ -66,150 +66,7 @@ namespace MomomaAssets
                 ctx.SetMainObject(texture2DArray);
             }
         }
+
+        public Texture2D GetTexture(int index) => m_Texture2Ds[index];
     }
-
-    [CustomEditor(typeof(Texture2DArrayImporter))]
-    public class Texture2DArrayImporterInspector : ScriptedImporterEditor
-    {
-        readonly static Type s_TextureInspectorType = Type.GetType("UnityEditor.TextureInspector, UnityEditor.dll");
-
-        public override bool showImportedObject => false;
-
-        ReorderableList m_ReorderableList;
-        Texture2D m_InitialTexture;
-        Editor m_TextureEditor;
-        Editor m_Texture2DArrayInspector;
-        Texture2D m_TempTexture;
-        SerializedProperty m_FilterMode;
-        SerializedProperty m_WrapModeU;
-        SerializedProperty m_WrapModeV;
-        SerializedProperty m_WrapModeW;
-        SerializedProperty m_AnisoLevel;
-        SerializedProperty m_ColorSpace;
-
-        public override void OnEnable()
-        {
-            base.OnEnable();
-            m_TempTexture = new Texture2D(2, 2);
-            m_FilterMode = serializedObject.FindProperty("m_FilterMode");
-            m_WrapModeU = serializedObject.FindProperty("m_WrapModeU");
-            m_WrapModeV = serializedObject.FindProperty("m_WrapModeV");
-            m_WrapModeW = serializedObject.FindProperty("m_WrapModeW");
-            m_AnisoLevel = serializedObject.FindProperty("m_AnisoLevel");
-            m_ColorSpace = serializedObject.FindProperty("m_ColorSpace");
-            SyncTempTexture();
-            m_ReorderableList = new ReorderableList(serializedObject, serializedObject.FindProperty("m_Texture2Ds"));
-            m_ReorderableList.drawHeaderCallback += r => EditorGUI.LabelField(r, "Textures");
-            m_ReorderableList.drawElementCallback += DrawElemnt;
-            m_ReorderableList.onReorderCallbackWithDetails += OnChanged;
-            m_ReorderableList.elementHeight = 64;
-            m_TextureEditor = Editor.CreateEditor(m_TempTexture, s_TextureInspectorType);
-        }
-
-        public override void OnDisable()
-        {
-            base.OnDisable();
-            if (m_TempTexture != null)
-                DestroyImmediate(m_TempTexture);
-            if (m_TextureEditor != null)
-                DestroyImmediate(m_TextureEditor);
-        }
-
-        public override void OnInspectorGUI()
-        {
-            Init();
-            using (var check = new EditorGUI.ChangeCheckScope())
-            {
-                m_TextureEditor.OnInspectorGUI();
-                if (check.changed)
-                {
-                    m_FilterMode.enumValueIndex = (int)m_TempTexture.filterMode;
-                    m_WrapModeU.enumValueIndex = (int)m_TempTexture.wrapModeU;
-                    m_WrapModeV.enumValueIndex = (int)m_TempTexture.wrapModeV;
-                    m_WrapModeW.enumValueIndex = (int)m_TempTexture.wrapModeW;
-                    m_AnisoLevel.intValue = m_TempTexture.anisoLevel;
-                }
-            }
-            EditorGUILayout.Separator();
-            m_ReorderableList.DoLayoutList();
-            ApplyRevertGUI();
-        }
-
-        protected override void Apply()
-        {
-            for (var index = m_ReorderableList.serializedProperty.arraySize - 1; index > -1; --index)
-            {
-                var element = m_ReorderableList.serializedProperty.GetArrayElementAtIndex(index);
-                if (element.objectReferenceValue == null)
-                    m_ReorderableList.serializedProperty.DeleteArrayElementAtIndex(index);
-            }
-            base.Apply();
-            SyncTempTexture();
-        }
-
-        protected override void ResetValues()
-        {
-            base.ResetValues();
-            SyncTempTexture();
-        }
-
-        void Init()
-        {
-            m_InitialTexture = null;
-            for (var index = 0; index < m_ReorderableList.serializedProperty.arraySize; ++index)
-            {
-                var tex = m_ReorderableList.serializedProperty.GetArrayElementAtIndex(index).objectReferenceValue as Texture2D;
-                if (tex != null)
-                {
-                    m_InitialTexture = tex;
-                    break;
-                }
-            }
-            if (m_InitialTexture != null)
-            {
-                using (var texSO = new SerializedObject(m_InitialTexture))
-                {
-                    m_ColorSpace.intValue = texSO.FindProperty("m_ColorSpace").intValue;
-                }
-            }
-        }
-
-        void SyncTempTexture()
-        {
-            if (m_TempTexture == null)
-                return;
-            m_TempTexture.filterMode = (FilterMode)m_FilterMode.enumValueIndex;
-            m_TempTexture.wrapModeU = (TextureWrapMode)m_WrapModeU.enumValueIndex;
-            m_TempTexture.wrapModeV = (TextureWrapMode)m_WrapModeV.enumValueIndex;
-            m_TempTexture.wrapModeW = (TextureWrapMode)m_WrapModeW.enumValueIndex;
-            m_TempTexture.anisoLevel = m_AnisoLevel.intValue;
-        }
-
-        void DrawElemnt(Rect rect, int index, bool isActive, bool isFocused)
-        {
-            var label = new GUIContent(string.Format("Element{0}", index));
-            var property = m_ReorderableList.serializedProperty.GetArrayElementAtIndex(index);
-            var tex = property.objectReferenceValue as Texture2D;
-            if (tex == null || m_InitialTexture == null || tex.width != m_InitialTexture.width || tex.height != m_InitialTexture.height || tex.format != m_InitialTexture.format || tex.mipmapCount != m_InitialTexture.mipmapCount)
-            {
-                var erroricon = EditorGUIUtility.IconContent("console.erroricon");
-                label.image = erroricon.image;
-            }
-            var r = rect;
-            r.width -= m_ReorderableList.elementHeight;
-            EditorGUI.LabelField(r, label);
-            r = rect;
-            r.xMin = r.xMax - m_ReorderableList.elementHeight;
-            EditorGUI.ObjectField(r, m_ReorderableList.serializedProperty.GetArrayElementAtIndex(index), typeof(Texture2D), GUIContent.none);
-        }
-
-        void OnChanged(ReorderableList list, int oldIndex, int newIndex)
-        {
-            list.serializedProperty.MoveArrayElement(newIndex, oldIndex);
-            list.serializedProperty.serializedObject.ApplyModifiedProperties();
-            list.serializedProperty.serializedObject.Update();
-            list.serializedProperty.MoveArrayElement(oldIndex, newIndex);
-        }
-    }
-
 }// namespace MomomaAssets
--- Editor/Texture2DArray/Texture2DArrayInspector.cs
+++ Editor/Texture2DArray/Texture2DArrayInspector.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Reflection;
-using UnityEngine;
 using UnityEditor;
+using UnityEngine;
 
 namespace MomomaAssets
 {
@@ -10,37 +10,22 @@ namespace MomomaAssets
     public class Texture2DArrayInspector : Editor
     {
         readonly static Type s_Texture2DArrayInspectorType = Type.GetType("UnityEditor.Texture2DArrayInspector, UnityEditor.dll");
-        readonly static Type s_TextureInspectorType = Type.GetType("UnityEditor.TextureInspector, UnityEditor.dll");
         readonly static FieldInfo s_SliceInfo = s_Texture2DArrayInspectorType.GetField("m_Slice", BindingFlags.NonPublic | BindingFlags.Instance);
 
-        Texture2DArray texture2DArray;
         Editor m_Texture2DArrayEditor;
         Editor m_PreviewTextureEditor;
-        Texture2D m_PreviewTexture;
-        int m_Slice = -1;
-        Texture2DArrayImporter m_Importer;
-        bool m_MipChain;
-        bool m_Linear;
 
         void OnEnable()
         {
-            texture2DArray = target as Texture2DArray;
             m_Texture2DArrayEditor = Editor.CreateEditor(target, s_Texture2DArrayInspectorType);
-            var path = AssetDatabase.GetAssetPath(target);
-            if (!string.IsNullOrEmpty(path))
-                m_Importer = AssetImporter.GetAtPath(path) as Texture2DArrayImporter;
-            m_MipChain = serializedObject.FindProperty("m_MipCount").intValue > 1;
-            m_Linear = serializedObject.FindProperty("m_ColorSpace").intValue == 0;
         }
 
         void OnDisable()
         {
-            if (m_Texture2DArrayEditor != null)
-                DestroyImmediate(m_Texture2DArrayEditor);
-            if (m_PreviewTextureEditor != null)
-                DestroyImmediate(m_PreviewTextureEditor);
-            if (m_PreviewTexture != null)
-                DestroyImmediate(m_PreviewTexture);
+            DestroyImmediate(m_Texture2DArrayEditor);
+            m_Texture2DArrayEditor = null;
+            DestroyImmediate(m_PreviewTextureEditor);
+            m_PreviewTextureEditor = null;
         }
 
         public override bool HasPreviewGUI()
@@ -56,26 +41,13 @@ namespace MomomaAssets
         public override void OnPreviewSettings()
         {
             var sliceInternal = (int)s_SliceInfo.GetValue(m_Texture2DArrayEditor);
-            if (m_PreviewTextureEditor == null || sliceInternal != m_Slice)
+            var importer = AssetImporter.GetAtPath(AssetDatabase.GetAssetPath(target)) as Texture2DArrayImporter;
+            if (importer != null)
             {
-                if (m_PreviewTextureEditor != null)
-                    DestroyImmediate(m_PreviewTextureEditor);
-                m_Slice = sliceInternal;
-                var slice = Mathf.Clamp(sliceInternal, 0, texture2DArray.depth - 1);
-                m_PreviewTexture = new Texture2D(texture2DArray.width, texture2DArray.height, texture2DArray.format, m_MipChain, m_Linear);
-                Graphics.CopyTexture(texture2DArray, slice, m_PreviewTexture, 0);
-                if (m_Importer != null)
-                {
-                    using (var texSO = new SerializedObject(m_PreviewTexture))
-                    using (var importerSO = new SerializedObject(m_Importer))
-                    {
-                        texSO.FindProperty("m_LightmapFormat").intValue = importerSO.FindProperty("m_LightmapFormat").intValue;
-                        texSO.ApplyModifiedPropertiesWithoutUndo();
-                    }
-                }
-                m_PreviewTextureEditor = Editor.CreateEditor(m_PreviewTexture, s_TextureInspectorType);
+                var currentTexture = importer.GetTexture(sliceInternal);
+                Editor.CreateCachedEditor(currentTexture, null, ref m_PreviewTextureEditor);
             }
-            m_PreviewTextureEditor.OnPreviewSettings();
+            m_PreviewTextureEditor?.OnPreviewSettings();
         }
 
         public override void OnPreviewGUI(Rect rect, GUIStyle background)
