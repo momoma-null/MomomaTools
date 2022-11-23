36204d265990d46ad33675e23a1ecc4d22d10d62
momoma <momoma.creation@gmail.com>
2022-11-23 18:30:19 +0900

    Add SceneVertexColorMode

---
+++ Editor/SceneVertexColorMode.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace MomomaAssets
+{
+    [InitializeOnLoad]
+    sealed class SceneVertexColorMode
+    {
+        static class Cache
+        {
+            public static readonly Shader vertexColorShader = Shader.Find("Hidden/MS_VertexColor");
+            public static readonly int vertexColorId = Shader.PropertyToID("_MS_VertexColor");
+        }
+
+        static readonly SceneView.CameraMode vertexRMode;
+        static readonly SceneView.CameraMode vertexGMode;
+        static readonly SceneView.CameraMode vertexBMode;
+        static readonly SceneView.CameraMode vertexAMode;
+        static readonly HashSet<SceneView> setupSceneViews = new HashSet<SceneView>();
+
+        static SceneVertexColorMode()
+        {
+            vertexRMode = SceneView.AddCameraMode("Vertex R", "Momoma Tools");
+            vertexGMode = SceneView.AddCameraMode("Vertex G", "Momoma Tools");
+            vertexBMode = SceneView.AddCameraMode("Vertex B", "Momoma Tools");
+            vertexAMode = SceneView.AddCameraMode("Vertex A", "Momoma Tools");
+            SceneView.beforeSceneGui += view =>
+            {
+                if (setupSceneViews.Add(view))
+                {
+                    view.onCameraModeChanged += cameraMode =>
+                    {
+                        if (cameraMode == vertexRMode)
+                        {
+                            view.SetSceneViewShaderReplace(Cache.vertexColorShader, string.Empty);
+                            Shader.SetGlobalColor(Cache.vertexColorId, Color.red);
+                        }
+                        else if (cameraMode == vertexGMode)
+                        {
+                            view.SetSceneViewShaderReplace(Cache.vertexColorShader, string.Empty);
+                            Shader.SetGlobalColor(Cache.vertexColorId, Color.green);
+                        }
+                        else if (cameraMode == vertexBMode)
+                        {
+                            view.SetSceneViewShaderReplace(Cache.vertexColorShader, string.Empty);
+                            Shader.SetGlobalColor(Cache.vertexColorId, Color.blue);
+                        }
+                        else if (cameraMode == vertexAMode)
+                        {
+                            view.SetSceneViewShaderReplace(Cache.vertexColorShader, string.Empty);
+                            Shader.SetGlobalColor(Cache.vertexColorId, Color.black);
+                        }
+                        else if (cameraMode.drawMode == DrawCameraMode.Textured)
+                        {
+                            view.SetSceneViewShaderReplace(null, string.Empty);
+                        }
+                    };
+                }
+            };
+        }
+    }
+
+}// namespace MomomaAssets
---
+++ Editor/Shaders/MS_VertexColor.shader
@@ -0,0 +1,43 @@
+﻿Shader "Hidden/MS_VertexColor"
+{
+    SubShader
+    {
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float4 color : COLOR;
+            };
+
+            struct v2f
+            {
+                float4 color : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            float4 _MS_VertexColor;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.color = _MS_VertexColor * v.color;
+                o.color = _MS_VertexColor.r == 0 && _MS_VertexColor.g == 0 && _MS_VertexColor.b == 0 ? v.color.a : o.color;
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                return i.color;
+            }
+            ENDCG
+        }
+    }
+}
