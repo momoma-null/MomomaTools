7c57cc05af934004122ca4965dca18a90bd8b851
momoma <momoma.creation@gmail.com>
2022-12-30 12:25:44 +0900

    Add LightProbesVisualiozer

---
+++ Editor/LightProbesVisualizer.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace MomomaAssets
+{
+    [InitializeOnLoad]
+    static class LightProbesVisualizer
+    {
+        const string menuPath = "MomomaTools/Light Probes Visualizer";
+        static readonly List<Matrix4x4> matrices = new List<Matrix4x4>();
+        static Mesh sphereMesh;
+        static Material diffuseMaterial;
+        static MaterialPropertyBlock materialPropertyBlock;
+
+        static LightProbesVisualizer()
+        {
+            EditorApplication.delayCall += () => Initialize();
+        }
+
+        [MenuItem(menuPath)]
+        static void ToggleLock()
+        {
+            var enabled = !Menu.GetChecked(menuPath);
+            Menu.SetChecked(menuPath, enabled);
+            SceneView.duringSceneGui -= OnSceneGUI;
+            if (enabled)
+                SceneView.duringSceneGui += OnSceneGUI;
+        }
+        static void Initialize()
+        {
+            var temp = ObjectFactory.CreatePrimitive(PrimitiveType.Sphere);
+            sphereMesh = temp.GetComponent<MeshFilter>().sharedMesh;
+            UnityEngine.Object.DestroyImmediate(temp);
+            diffuseMaterial = new Material(Shader.Find("Hidden/MS_LightProbes")) { hideFlags = HideFlags.HideAndDontSave, enableInstancing = true };
+            materialPropertyBlock = new MaterialPropertyBlock();
+            LightProbes.tetrahedralizationCompleted += () => RecalculateMatrices();
+            EditorSceneManager.activeSceneChangedInEditMode += (x, y) => RecalculateMatrices();
+            RecalculateMatrices();
+        }
+
+        static void OnSceneGUI(SceneView view)
+        {
+            Graphics.DrawMeshInstanced(sphereMesh, 0, diffuseMaterial, matrices, materialPropertyBlock, ShadowCastingMode.Off, false, 0, view.camera, LightProbeUsage.CustomProvided);
+        }
+
+        static void RecalculateMatrices()
+        {
+            matrices.Clear();
+            matrices.AddRange(LightmapSettings.lightProbes.positions.Select(pos => Matrix4x4.TRS(pos, Quaternion.identity, 0.1f * Vector3.one)));
+            materialPropertyBlock.CopySHCoefficientArraysFrom(LightmapSettings.lightProbes.bakedProbes);
+        }
+    }
+}
---
+++ Editor/Shaders/MS_LightProbes.shader
@@ -0,0 +1,53 @@
+﻿Shader "Hidden/MS_LightProbes"
+{
+    SubShader
+    {
+        Pass
+        {
+            Name "FORWARD"
+            Tags { "LightMode" = "ForwardBase" }
+
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            #pragma multi_compile LIGHTPROBE_SH
+            #pragma multi_compile_instancing
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float3 normal : NORMAL;
+                UNITY_VERTEX_INPUT_INSTANCE_ID
+            };
+
+            struct v2f
+            {
+                float3 wNormal : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+                UNITY_VERTEX_INPUT_INSTANCE_ID
+                UNITY_VERTEX_OUTPUT_STEREO
+            };
+
+            v2f vert (appdata v)
+            {
+                UNITY_SETUP_INSTANCE_ID(v);
+                v2f o;
+                UNITY_INITIALIZE_OUTPUT(v2f, o);
+                UNITY_TRANSFER_INSTANCE_ID(v, o);
+                UNITY_INITIALIZE_VERTEX_OUTPUT_STEREO(o);
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.wNormal = UnityObjectToWorldNormal(v.normal);
+                return o;
+            }
+
+            fixed4 frag (v2f IN) : SV_Target
+            {
+                UNITY_SETUP_INSTANCE_ID(IN);
+                return half4(ShadeSH9(half4(IN.wNormal, 1)), 1);
+            }
+            ENDCG
+        }
+    }
+}
